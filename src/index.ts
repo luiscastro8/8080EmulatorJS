97bba14ec176028ec500b0f185e7b33aa26b08e9
luiscastro8 <ochoad64@gmail.com>
2021-10-09 12:55:06 -0500

    linted code

--- src/index.ts
+++ src/index.ts
@@ -2,23 +2,23 @@ const canvas = <HTMLCanvasElement>document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 const imageData = ctx.createImageData(100, 100);
 
-const fileInput = <HTMLInputElement>document.getElementById('fileinput');
+const fileInput = < HTMLInputElement>document.getElementById('fileinput');
 fileInput.addEventListener('change', (e) => {
-    const reader = new FileReader();
-    reader.onloadend = () => {
-        console.log(reader.result);
-    }
-    const file = (<HTMLInputElement>e.target).files[0];
-    reader.readAsArrayBuffer(file);
-})
+  const reader = new FileReader();
+  reader.onloadend = () => {
+    // console.log(reader.result);
+  };
+  const file = (<HTMLInputElement>e.target).files[0];
+  reader.readAsArrayBuffer(file);
+});
 
 // Iterate through every pixel
 for (let i = 0; i < imageData.data.length; i += 4) {
-    // Modify pixel data
-    imageData.data[i + 0] = 190;  // R value
-    imageData.data[i + 1] = 0;    // G value
-    imageData.data[i + 2] = 210;  // B value
-    imageData.data[i + 3] = 255;  // A value
+  // Modify pixel data
+  imageData.data[i + 0] = 190; // R value
+  imageData.data[i + 1] = 0; // G value
+  imageData.data[i + 2] = 210; // B value
+  imageData.data[i + 3] = 255; // A value
 }
 
 // Draw image data to the canvas
