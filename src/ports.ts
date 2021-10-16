d3674e984c48e300926f4403ad19c8e92e85fee3
luiscastro8 <ochoad64@gmail.com>
2021-10-16 10:36:50 -0500

    added ports

--- src/ports.ts
+++ src/ports.ts
@@ -1,11 +1,4 @@
 export default class Ports {
-  // uint8_t r0 : 8;
-  // uint8_t r1 : 8;
-  // uint8_t r2 : 8;
-  // uint8_t w3 : 8;
-  // uint8_t w5 : 8;
-  // uint8_t w6 : 8;
-
   private r0: number;
 
   private r1: number;
@@ -17,4 +10,40 @@ export default class Ports {
   private w5: number;
 
   private w6: number;
+
+  public getR0 = () => this.r0;
+
+  public getR1 = () => this.r1;
+
+  public getR2 = () => this.r2;
+
+  public getW3 = () => this.w3;
+
+  public getW5 = () => this.w5;
+
+  public getW6 = () => this.w6;
+
+  public setR0 = (num: number) => {
+    this.r0 = num & 0xff;
+  };
+
+  public setR1 = (num: number) => {
+    this.r1 = num & 0xff;
+  };
+
+  public setR2 = (num: number) => {
+    this.r2 = num & 0xff;
+  };
+
+  public setW3 = (num: number) => {
+    this.w3 = num & 0xff;
+  };
+
+  public setW5 = (num: number) => {
+    this.w5 = num & 0xff;
+  };
+
+  public setW6 = (num: number) => {
+    this.w6 = num & 0xff;
+  };
 }
