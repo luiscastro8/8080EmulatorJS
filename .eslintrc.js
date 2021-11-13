7ed812d780c52028fe3d82951d5b2c7fd10f9577
luiscastro8 <ochoad64@gmail.com>
2021-11-12 22:34:16 -0600

    added airbnb typescript lint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,16 +4,21 @@ module.exports = {
     es2021: true,
     "jest/globals": true,
   },
-  extends: ["airbnb-base", "prettier", "plugin:import/typescript"],
+  extends: [
+    "airbnb-base",
+    "airbnb-typescript/base",
+    "prettier",
+    "plugin:import/typescript",
+  ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: 12,
     sourceType: "module",
+    project: "./tsconfig.json",
   },
   plugins: ["@typescript-eslint", "jest"],
   rules: {
     "no-bitwise": ["error", { allow: ["&", "|", "<<", ">>"] }],
     "prefer-destructuring": ["error", { object: true, array: false }],
-    "import/extensions": ["error", "ignorePackages", { ts: "never" }],
   },
 };
