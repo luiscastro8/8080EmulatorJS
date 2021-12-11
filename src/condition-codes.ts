127da62f571104bdd4805abaf77cb6e3dfd2f461
luiscastro8 <ochoad64@gmail.com>
2021-12-11 15:59:55 -0600

    added set flags function

--- src/condition-codes.ts
+++ src/condition-codes.ts
@@ -9,4 +9,25 @@ export default class ConditionCodes {
 
   public ac: boolean = false;
   // TODO see if pad is needed
+
+  public setFlags = (answer: number, changeCarry: boolean) => {
+    this.z = (answer & 0xff) === 0;
+    this.s = (answer & 0x80) !== 0;
+    this.p = ConditionCodes.getParity(answer & 0xff) % 2 === 0;
+    if (changeCarry) {
+      this.cy = answer > 0xff || answer < 0;
+    }
+  };
+
+  public static getParity = (val: number) => {
+    let count = 0;
+    let tempVal = val;
+    while (tempVal !== 0) {
+      if (tempVal % 2 === 1) {
+        count += 1;
+      }
+      tempVal = Math.floor(tempVal / 2);
+    }
+    return count;
+  };
 }
---
+++ test/conditionCodes.spec.ts
@@ -0,0 +1,43 @@
+import ConditionCodes from "../src/condition-codes";
+
+test("zero flag", () => {
+  const cc = new ConditionCodes();
+  cc.setFlags(0, false);
+  expect(cc.z).toBe(true);
+  cc.setFlags(2, false);
+  expect(cc.z).toBe(false);
+});
+
+test("sign flag", () => {
+  const cc = new ConditionCodes();
+  cc.setFlags(0b10000000, false);
+  expect(cc.s).toBe(true);
+  cc.setFlags(0b01111111, false);
+  expect(cc.s).toBe(false);
+});
+
+test("parity flag", () => {
+  const cc = new ConditionCodes();
+  cc.setFlags(0b011011, false);
+  expect(cc.p).toBe(true);
+  cc.setFlags(0b10101, false);
+  expect(cc.p).toBe(false);
+});
+
+test("carry flag", () => {
+  const cc = new ConditionCodes();
+  cc.setFlags(0xfff, false);
+  expect(cc.cy).toBe(false);
+  cc.setFlags(0xfff, true);
+  expect(cc.cy).toBe(true);
+  cc.setFlags(0xf, true);
+  expect(cc.cy).toBe(false);
+});
+
+test("getParity", () => {
+  expect(ConditionCodes.getParity(0b000)).toBe(0);
+  expect(ConditionCodes.getParity(0b001)).toBe(1);
+  expect(ConditionCodes.getParity(0b010)).toBe(1);
+  expect(ConditionCodes.getParity(0b011)).toBe(2);
+  expect(ConditionCodes.getParity(0b100)).toBe(1);
+});
