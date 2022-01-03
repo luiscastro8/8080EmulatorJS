36a09ce3815330070202d2d33ec675e9a64c3dbc
luiscastro8 <ochoad64@gmail.com>
2022-01-02 19:22:30 -0600

    made display work

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import initializeEmulator from "./initializeEmulator";
 
 const canvas = <HTMLCanvasElement>document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
-const imageData = ctx.createImageData(100, 100);
+const imageData = ctx.createImageData(224, 256);
 
 const fileInput = <HTMLInputElement>document.getElementById("fileinput");
 fileInput.addEventListener("change", (event) => {
@@ -16,11 +16,11 @@ fileInput.addEventListener("change", (event) => {
 // Iterate through every pixel
 for (let i = 0; i < imageData.data.length; i += 4) {
   // Modify pixel data
-  imageData.data[i + 0] = 190; // R value
+  imageData.data[i + 0] = 0; // R value
   imageData.data[i + 1] = 0; // G value
-  imageData.data[i + 2] = 210; // B value
+  imageData.data[i + 2] = 0; // B value
   imageData.data[i + 3] = 255; // A value
 }
 
 // Draw image data to the canvas
-ctx.putImageData(imageData, 20, 20);
+ctx.putImageData(imageData, 0, 0);
--- src/initializeEmulator.ts
+++ src/initializeEmulator.ts
@@ -10,6 +10,35 @@ function sleep(ms: number) {
   });
 }
 
+const updateDisplay = () => {
+  const ctx = (
+    document.getElementById("canvas") as HTMLCanvasElement
+  ).getContext("2d");
+  const imageData = ctx.getImageData(0, 0, 224, 256);
+  let videoPointer = 0x2400;
+  for (let w = 0; w < 224; w += 1) {
+    for (let h = 31; h >= 0; h -= 1) {
+      let byte = state.memory[videoPointer];
+      for (let b = 0; b < 8; b += 1) {
+        if (byte % 2 === 0) {
+          imageData.data[((h + 1) * 1792 - b * 224 + w) * 4 + 0] = 0;
+          imageData.data[((h + 1) * 1792 - b * 224 + w) * 4 + 1] = 0;
+          imageData.data[((h + 1) * 1792 - b * 224 + w) * 4 + 2] = 0;
+          imageData.data[((h + 1) * 1792 - b * 224 + w) * 4 + 3] = 255;
+        } else {
+          imageData.data[((h + 1) * 1792 - b * 224 + w) * 4 + 0] = 255;
+          imageData.data[((h + 1) * 1792 - b * 224 + w) * 4 + 1] = 0;
+          imageData.data[((h + 1) * 1792 - b * 224 + w) * 4 + 2] = 0;
+          imageData.data[((h + 1) * 1792 - b * 224 + w) * 4 + 3] = 255;
+        }
+        byte = Math.floor(byte / 2);
+      }
+      videoPointer += 1;
+    }
+  }
+  ctx.putImageData(imageData, 0, 0);
+};
+
 const processInterrupt = async () => {
   if (state.enableInterrupt) {
     state.memory[state.sp - 1] = (state.pc >> 8) & 0xff;
@@ -27,7 +56,7 @@ const processInterrupt = async () => {
         await sleep(1000);
       }
       state.interruptPointer = 0x10;
-      // TODO update display
+      updateDisplay();
     }
   }
 };
@@ -42,7 +71,7 @@ const initializeEmulator = async (loadFileEvent: ProgressEvent<FileReader>) => {
   }
 
   for (let i = 0; i < 100000; i += 1) {
-    if (i > 37300) {
+    if (i > 40000) {
       /* eslint-disable-next-line no-console */
       console.log(
         `${i}: emulating instruction 0x${Number(
