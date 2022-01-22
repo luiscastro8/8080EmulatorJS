721954b90903f6dc79349c865d7763c268124090
luiscastro8 <ochoad64@gmail.com>
2022-01-22 13:40:40 -0600

    work on rendering

--- src/initializeEmulator.ts
+++ src/initializeEmulator.ts
@@ -8,68 +8,51 @@ let count: number = 0;
 let frameBuffer: Uint32Array;
 
 const updateDisplay = () => {
-  const ctx = (
-    document.getElementById("canvas") as HTMLCanvasElement
-  ).getContext("2d");
-  const imageData = ctx.getImageData(0, 0, 224, 256);
   let videoPointer = 0x2400;
-  // let minX = 223;
-  // let minY = 255;
-  // let maxX = 0;
-  // let maxY = 0;
-
+  let minX = -1;
+  let minY = -1;
+  let maxX = -1;
+  let maxY = -1;
   for (let w = 0; w < 224; w += 1) {
     for (let h = 31; h >= 0; h -= 1) {
       let byte = state.memory[videoPointer];
       for (let b = 0; b < 8; b += 1) {
-        // if (frameBuffer[(h + 1) * (224 * 8) - b * 224 + w] !== byte % 2) {
-        //   if (w < minX) {
-        //     w = minX
-        //   }
-        //   if (w > maxX) {
-        //     w = maxX;
-        //   }
-        //   if ((h + 1) * 8 - b < minY) {
-        //     minY = (h + 1) * 8 - b;
-        //   }
-        //   if ((h + 1) * 8 - b > maxY) {
-        //     maxY = (h + 1) * 8 - b;
-        //   }
-        // }
-        frameBuffer[(h + 1) * (224 * 8) - b * 224 + w] = byte % 2;
+        if (
+          frameBuffer[(h + 1) * (224 * 8) - (b * 224 + 224) + w] !==
+          byte % 2
+        ) {
+          if (w < minX || minX === -1) {
+            minX = w;
+          }
+          if (w > maxX || maxX === -1) {
+            maxX = w;
+          }
+          if ((h + 1) * 8 - b < minY || minY === -1) {
+            minY = (h + 1) * 8 - b;
+          }
+          if ((h + 1) * 8 - b > maxY || maxY === -1) {
+            maxY = (h + 1) * 8 - b;
+          }
+          frameBuffer[(h + 1) * (224 * 8) - (b * 224 + 224) + w] = byte % 2;
+        }
+
         byte = Math.floor(byte / 2);
       }
       videoPointer += 1;
     }
   }
 
+  const ctx = (
+    document.getElementById("canvas") as HTMLCanvasElement
+  ).getContext("2d");
+  // const imageData = ctx.getImageData(minX, minY, maxX - minX, maxY - minY);
+  const imageData = ctx.getImageData(0, 0, 224, 256);
   for (let i = 0; i < 224 * 256; i += 1) {
     imageData.data[i * 4 + 0] = frameBuffer[i] === 0 ? 0 : 255;
     imageData.data[i * 4 + 1] = 0;
     imageData.data[i * 4 + 2] = 0;
     imageData.data[i * 4 + 3] = 255;
   }
-
-  // for (let w = 0; w < 224; w += 1) {
-  //   for (let h = 31; h >= 0; h -= 1) {
-  //     let byte = state.memory[videoPointer];
-  //     for (let b = 0; b < 8; b += 1) {
-  //       if (byte % 2 === 0) {
-  //         imageData.data[((h + 1) * 1792 - b * 224 + w) * 4 + 0] = 0;
-  //         imageData.data[((h + 1) * 1792 - b * 224 + w) * 4 + 1] = 0;
-  //         imageData.data[((h + 1) * 1792 - b * 224 + w) * 4 + 2] = 0;
-  //         imageData.data[((h + 1) * 1792 - b * 224 + w) * 4 + 3] = 255;
-  //       } else {
-  //         imageData.data[((h + 1) * 1792 - b * 224 + w) * 4 + 0] = 255;
-  //         imageData.data[((h + 1) * 1792 - b * 224 + w) * 4 + 1] = 0;
-  //         imageData.data[((h + 1) * 1792 - b * 224 + w) * 4 + 2] = 0;
-  //         imageData.data[((h + 1) * 1792 - b * 224 + w) * 4 + 3] = 255;
-  //       }
-  //       byte = Math.floor(byte / 2);
-  //     }
-  //     videoPointer += 1;
-  //   }
-  // }
   ctx.putImageData(imageData, 0, 0);
 };
 
@@ -140,6 +123,9 @@ const initializeEmulator = async (loadFileEvent: ProgressEvent<FileReader>) => {
   frameStartTime = performance.now();
   state = new State8080();
   frameBuffer = new Uint32Array(224 * 256);
+  for (let i = 0; i < frameBuffer.length; i += 1) {
+    frameBuffer[i] = 0;
+  }
   for (let i = 0; i < romBuffer.length; i += 1) {
     state.memory[i] = romBuffer[i];
   }
